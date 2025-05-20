55067e9bf8dd9338a03decec2e97a8d3dc6aa484
maaz8883 <maaz.staffshaw@gmail.com>
2025-05-20 14:06:43 -0700

    header changes

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -21,7 +21,7 @@
             </div>
             <div class="col-md-7">
                 <div class="row">
-                    <div class="col-md-3 h-icon-col">
+                    <div class="col-md-4 h-icon-col">
                         <div class="h-icon">
                             <span><i class="fa-solid fa-phone-volume"></i></span>
                         </div>
@@ -37,7 +37,7 @@
                             @endif
                         </div>
                     </div>
-                    <div class="col-md-6 h-icon-col">
+                    <div class="col-md-5 h-icon-col">
                         <div class="h-icon">
                             <span><i class="fa-solid fa-envelope"></i></span>
                         </div>
